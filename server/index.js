a23da53c7ed428df654d706a0a33e620008b34c6
Alex-cr17 <Alex-cr17@mail.ru>
2018-11-29 13:34:27 +0200

    Init commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import path from 'path';
+let app = express();
+
+const PORT = 3000;
+app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(PORT, () => console.log(`Running on localhost:${PORT}`));
