a75fd3fcbc404cd4413ea57c63d0a545d8c5c945
Alex-cr17 <Alex-cr17@mail.ru>
2018-11-29 19:01:27 +0200

    Install react and render first component

---
+++ client/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+export default () => {
+      return (
+            <h1>sd asd asdHello</h1>
+      )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+      devtools: 'eval-source-map',
+      mode: "development",
+      entry: path.join(__dirname, '/client/index.js'),
+      output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+      },
       module: {
             rules: [
-                  { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+                  {
+                        test: /\.js$/,
+                        use: 'babel-loader',
+                  },
+                  {
+                        test: /\.css$/,
+                        use: ['style-loader', 'css-loader']
+                  }
             ]
-      }
+      },
+      plugins: [
+            new HtmlWebpackPlugin({
+                  template: 'server/index.html'
+            })
+      ]
 }
